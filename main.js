e07876895606df51b0d80e8f730e2378693e6a1b
Shashank Srivastava <shashank.srivastava@claysol.com>
2021-05-23 12:53:27 +0530

    finished main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-let args = process.argv.slice(2);
+const argv = require('minimist')(process.argv.slice(2));
+const path = require('path');
 
-args.forEach((val, index) => {
-    console.log(`${index}: ${val}`)
-});
+const Query = require('./lib/Query');
+
+let inputFilePath = argv.d || argv.data;
+let input;
+
+if (inputFilePath) {
+    if (path.isAbsolute(inputFilePath)) {
+        input = require(inputFilePath);
+    }
+    else {
+        input = require('./'+inputFilePath);
+    }
+}
+
+let queryObj = new Query(input);
+console.log(queryObj.queryString);
